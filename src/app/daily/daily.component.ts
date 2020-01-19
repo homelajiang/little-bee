faac47dccd9834a7f0797dd3ec2aba6e67e32f42
liangxiyuan <liangxiyuan@kedacom.com>
2020-01-19 17:58:37 +0800

    搞定浮层

--- src/app/daily/daily.component.ts
+++ src/app/daily/daily.component.ts
@@ -22,7 +22,7 @@ export class DailyComponent implements OnInit, AfterViewInit {
   @ViewChild('originFab', {static: false}) originFab: MatButton;
   private overlayRef: OverlayRef;
 
-  constructor(public overlay: Overlay, private viewContainerRef: ViewContainerRef) {
+  constructor(public overlay: Overlay, private viewContainerRef: ViewContainerRef, private elementRef: ElementRef) {
   }
 
   ngOnInit() {
@@ -33,79 +33,68 @@ export class DailyComponent implements OnInit, AfterViewInit {
 
   }
 
-  createMission(element: any) {
+  newMission(event: any) {
     if (this.overlayRef && this.overlayRef.hasAttached()) {
       this.overlayRef.detach();
-    } else {
-      const positionStrategy = this.overlay.position()
-        .flexibleConnectedTo(this.originFab._elementRef)
-        .withPositions([
-          {
-            originX: 'start',
-            originY: 'bottom',
-            overlayX: 'start',
-            overlayY: 'top'
-          }
-        ]);
-
-      this.overlayRef = this.overlay.create({
-        positionStrategy,
-        scrollStrategy: this.overlay.scrollStrategies.reposition(),
-        // direction: this.dir.valid,
-        // minHeight: 100,
-        hasBackdrop: true
-      });
-      this.overlayRef.attach(new ComponentPortal(NewTodoComponent, this.viewContainerRef));
     }
-  }
-
-  newMission(element: any) {
-    element = element.target;
-    // const viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    // const isBelowMax = viewPortHeight <= 300;
-    // const maxHeight = isBelowMax ? viewPortHeight : 300;
-    const overlayRef = this.overlay.create({
-      hasBackdrop: false,
+    const target = {
+      getBoundingClientRect: (): ClientRect => ({
+        bottom: event.clientY - event.offsetY + event.target.offsetHeight,
+        height: 0,
+        left: event.clientX - event.offsetX,
+        right: event.clientX - event.offsetX + event.target.offsetWidth,
+        top: event.clientY - event.offsetY,
+        width: 0,
+      }),
+    };
+    const element = new ElementRef(target);
+    this.overlayRef = this.overlay.create({
+      backdropClass: 'transparent-drop-bg',
+      hasBackdrop: true,
       positionStrategy: this.getFlexiblePosition(element),
-      // positionStrategy: positionStrategy,
-      // height: viewPortHeight,
-      // minHeight: viewPortHeight,
-      // height: viewPortHeight,
-      // width: '200px',
       maxHeight: 300,
     });
     const popupComponentPortal = new ComponentPortal(NewTodoComponent);
-    const componentRef = overlayRef.attach(popupComponentPortal);
+    this.overlayRef.attach(popupComponentPortal);
+    this.overlayRef.backdropClick().subscribe(() => this.overlayRef.dispose());
   }
 
-
   getFlexiblePosition(element: any) {
-    return this.overlay.position().flexibleConnectedTo(element._elementRef)
+    return this.overlay.position().flexibleConnectedTo(element)
       .withLockedPosition()
       // .withFlexibleDimensions(true)
       .withPush(true)
       .withPositions([
         {
-          panelClass: 'custom-panel1',
+          originX: 'end',
+          originY: 'top',
+          overlayX: 'start',
+          overlayY: 'top',
+          offsetX: 10,
+        },
+        {
           originX: 'start',
           originY: 'top',
           overlayX: 'end',
           overlayY: 'top',
+          offsetX: -10,
         },
         {
-          panelClass: 'custom-panel2',
-          originX: 'center',
-          originY: 'center',
-          overlayX: 'start',
-          overlayY: 'center',
-        },
-        {
-          panelClass: 'custom-panel3',
-          originX: 'center',
-          originY: 'center',
+          originX: 'end',
+          originY: 'bottom',
           overlayX: 'start',
           overlayY: 'bottom',
+          offsetX: 10,
+          offsetY: -10
         },
+        {
+          originX: 'start',
+          originY: 'bottom',
+          overlayX: 'end',
+          overlayY: 'bottom',
+          offsetX: -10,
+          offsetY: -10
+        }
       ]);
   }
 }
