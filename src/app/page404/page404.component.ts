608b32fc511cc5dca4c9573c62640b50910cd070
homelajiang <homelajiang@163.com>
2020-10-26 01:23:07 +0800

    添加404页面

--- src/app/page404/page404.component.ts
+++ src/app/page404/page404.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-page404',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page404Component implements OnInit {
 
-  constructor() { }
+  active = false;
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+    setTimeout(() => {
+      // document.querySelector('.cont_principal').className = 'cont_principal cont_error_active';
+      this.active = true;
+    }, 500);
+  }
+
+  toHome() {
+    this.router.navigate(['/']);
   }
 
 }
