d87fa5a3aed68c212ca2763b884a6d62a50deda9
liangxiyuan <liangxiyuan@kedacom.com>
2021-04-15 21:46:49 +0800

    bug fix

--- src/app/work-hour/work-hour.component.ts
+++ src/app/work-hour/work-hour.component.ts
@@ -3,43 +3,43 @@ import {BeeService, RankUser} from '../bee/bee.service';
 import {getQuarter} from 'date-fns';
 
 @Component({
-  selector: 'app-work-hour',
-  templateUrl: './work-hour.component.html',
-  styleUrls: ['./work-hour.component.css']
+    selector: 'app-work-hour',
+    templateUrl: './work-hour.component.html',
+    styleUrls: ['./work-hour.component.css']
 })
 export class WorkHourComponent implements OnInit {
 
-  searchType = '1'// 1 月 2 季度 3 年
-  searchDate = new Date()
-  quarter = getQuarter(this.searchDate) // 季度
-  userRanking: RankUser
-  rankingUsers:RankUser[]
-  ranking = '--'
-  workHours = '--'
+    searchType = '1'// 1 月 2 季度 3 年
+    searchDate = new Date()
+    quarter = getQuarter(this.searchDate) // 季度
+    userRanking: RankUser
+    rankingUsers: RankUser[]
+    ranking = '--'
+    workHours = '--'
 
-  constructor(private beeService: BeeService) {
-  }
+    constructor(private beeService: BeeService) {
+    }
 
-  ngOnInit(): void {
-    this.getRanking()
-  }
+    ngOnInit(): void {
+        this.getRanking()
+    }
 
-  changeSearchType() {
-    this.getRanking()
-  }
+    changeSearchType() {
+        this.getRanking()
+    }
 
-  private getRanking() {
-    this.beeService.getHourRanking(this.searchType, this.searchDate, this.quarter)
-      .subscribe(res => {
-        res.forEach((value, index) => {
-          if (value.userId === this.beeService.userInfo.id) {
-            this.userRanking = value
-            this.rankingUsers = res
-            this.ranking = index.toString()
-            this.workHours = value.workHours.toString()
-          }
-        })
-      })
-  }
+    private getRanking() {
+        this.beeService.getHourRanking(this.searchType, this.searchDate, this.quarter)
+            .subscribe(res => {
+                res.forEach((value, index) => {
+                    if (value.userId === this.beeService.userInfo.id) {
+                        this.userRanking = value
+                        this.rankingUsers = res
+                        this.ranking = (index + 1).toString()
+                        this.workHours = value.workHours.toString()
+                    }
+                })
+            })
+    }
 
 }
