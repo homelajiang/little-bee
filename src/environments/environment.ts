6f6f7f322747715e8336075706a9590519995388
homelajiang <homelajiang@163.com>
2022-12-30 00:35:48 +0800

    自定义样式

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
  * import package.json
  * https://stackoverflow.com/questions/34907682/how-to-display-the-app-version-in-angular/48869478#48869478
  */
-import pkg from '../../../package.json'
+import pkg from '../../package.json'
 
 export const environment = {
   version: `${pkg.version}-dev`,
